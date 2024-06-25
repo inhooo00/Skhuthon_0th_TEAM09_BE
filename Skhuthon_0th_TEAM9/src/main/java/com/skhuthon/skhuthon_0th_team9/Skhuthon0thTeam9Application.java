20eac2adace36f6aafed01aa8f4ab968135f8719
woogym <kwj9294@gmail.com>
2024-06-25 12:27:20 +0900

    [setting] : initialize setting

---
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/Skhuthon0thTeam9Application.java
@@ -0,0 +1,13 @@
+package com.skhuthon.skhuthon_0th_team9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skhuthon0thTeam9Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Skhuthon0thTeam9Application.class, args);
+    }
+
+}

d3ca22af2cb220f3feaf2dc417ca93a5d39660ab
woogym <kwj9294@gmail.com>
2024-06-27 00:17:48 +0900

    chore : add application build option

--- Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/Skhuthon0thTeam9Application.java
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/Skhuthon0thTeam9Application.java
@@ -2,7 +2,11 @@ package com.skhuthon.skhuthon_0th_team9;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableCaching
+@EnableJpaAuditing
 @SpringBootApplication
 public class Skhuthon0thTeam9Application {
 

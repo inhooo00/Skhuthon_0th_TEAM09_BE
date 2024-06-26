ce07bef2088584401591768234681d2221fe7d38
woogym <kwj9294@gmail.com>
2024-06-27 02:55:04 +0900

    chore : default timezone setting in application run class

--- Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/Skhuthon0thTeam9Application.java
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/Skhuthon0thTeam9Application.java
@@ -5,11 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import java.util.TimeZone;
+
 @EnableCaching
 @EnableJpaAuditing
 @SpringBootApplication
 public class Skhuthon0thTeam9Application {
     public static void main(String[] args) {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Seoul"));
         SpringApplication.run(Skhuthon0thTeam9Application.class, args);
     }
 

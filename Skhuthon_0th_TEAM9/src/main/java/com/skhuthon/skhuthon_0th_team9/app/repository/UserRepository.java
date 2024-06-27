54c1cbbc1dd091d761f30fabe86a5c8bd58fe827
woogym <kwj9294@gmail.com>
2024-06-28 00:51:30 +0900

    feature : add jpaRepository in UserRepository

--- Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/repository/UserRepository.java
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/repository/UserRepository.java
@@ -1,4 +1,8 @@
 package com.skhuthon.skhuthon_0th_team9.app.repository;
 
-public interface UserRepository {
+import com.skhuthon.skhuthon_0th_team9.app.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
 }

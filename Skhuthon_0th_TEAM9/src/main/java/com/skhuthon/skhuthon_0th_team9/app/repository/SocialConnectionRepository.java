59e5781732c73bfb81ce43b9d6da4d8b3896db1b
woogym <kwj9294@gmail.com>
2024-06-28 01:05:38 +0900

    feature :  update repository

--- Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/repository/SocialConnectionRepository.java
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/repository/SocialConnectionRepository.java
@@ -1,4 +1,7 @@
 package com.skhuthon.skhuthon_0th_team9.app.repository;
 
-public interface SocialConnectionRepository {
+import com.skhuthon.skhuthon_0th_team9.app.domain.user.social.SocialConnection;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SocialConnectionRepository extends JpaRepository<SocialConnection, Long> {
 }

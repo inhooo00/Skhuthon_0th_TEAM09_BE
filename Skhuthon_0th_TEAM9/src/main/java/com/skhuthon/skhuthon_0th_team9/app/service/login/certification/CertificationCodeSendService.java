9e79957b0740db45f38677f915b8e39982c37ce3
woogym <kwj9294@gmail.com>
2024-06-28 02:39:05 +0900

    feature : emailCertificationSendService

---
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/service/login/certification/CertificationCodeSendService.java
@@ -0,0 +1,18 @@
+package com.skhuthon.skhuthon_0th_team9.app.service.login.certification;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+import java.util.Random;
+
+@Service
+public class CertificationCodeSendService {
+
+    @Cacheable(cacheNames = "emailCertificationCode", value = "emailCertificationCode", key = "#email")
+    public String getCertificationCode(String email) {
+        return String.format("%06d", new Random().nextInt(1000000));
+    }
+
+    @Cacheable(cacheNames = "emailCertificationCode", value = "emailCertificationCode", key = "#email")
+    public void deleteCertificationCode(String email) {}
+}

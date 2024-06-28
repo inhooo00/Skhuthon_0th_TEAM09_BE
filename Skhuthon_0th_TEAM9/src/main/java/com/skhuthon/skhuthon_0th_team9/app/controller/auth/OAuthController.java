6bfc08107a54fdf53c1ec9c2af4b391112b9c46e
woogym <kwj9294@gmail.com>
2024-06-29 01:08:40 +0900

    fix : modify social login callback url

--- Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/controller/auth/OAuthController.java
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/controller/auth/OAuthController.java
@@ -13,7 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("/api/v1/oauth")
+@RequestMapping("/api/v1/oauth2")
 public class OAuthController {
 
     private final KakaoOAuthService kakaoOAuthService;

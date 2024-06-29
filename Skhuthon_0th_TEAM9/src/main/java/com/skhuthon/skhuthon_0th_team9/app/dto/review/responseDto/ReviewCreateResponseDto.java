abb8f0b8f0f5be33356c240f6013a8875b54cdce
woogym <kwj9294@gmail.com>
2024-06-29 22:41:26 +0900

    feature : ReviewCreateDto

---
+++ Skhuthon_0th_TEAM9/src/main/java/com/skhuthon/skhuthon_0th_team9/app/dto/review/responseDto/ReviewCreateResponseDto.java
@@ -0,0 +1,26 @@
+package com.skhuthon.skhuthon_0th_team9.app.dto.review.responseDto;
+
+import com.skhuthon.skhuthon_0th_team9.app.domain.category.DepartmentCategory;
+import com.skhuthon.skhuthon_0th_team9.app.domain.category.DetailCategory;
+import com.skhuthon.skhuthon_0th_team9.app.domain.user.User;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Builder
+public class ReviewCreateResponseDto {
+
+    private Long id;
+    private String title;
+    private String description;
+    private List<String> imageUrls;
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
+    private User user;
+    private DepartmentCategory departmentCategory;
+    private DetailCategory detailCategory;
+
+}

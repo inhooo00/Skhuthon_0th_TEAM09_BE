22e1961818b5d7ffc5759712999c2fd4cc733c0e
inhooo00 <chldlsgh0987@naver.com>
2024-07-03 01:25:55 +0900

    JWT토큰 + 리프레시 토큰 방식 구현

--- src/main/java/com/skhuthon/skhuthon_0th_team9/global/common/exception/ErrorCode.java
+++ src/main/java/com/skhuthon/skhuthon_0th_team9/global/common/exception/ErrorCode.java
@@ -60,8 +60,7 @@ public enum ErrorCode {
     FAILED_UPLOAD_IMAGE_FILE_EXCEPTION(HttpStatus.INTERNAL_SERVER_ERROR, "이미지 업로드에 실패했습니다"),
 
     // 503 Service Unavailable
-    FAILED_GET_TOKEN_EXCEPTION(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS, "구글 엑세스 토큰을 가져오는데 실패했습니다."),
-    ;
+    FAILED_GET_TOKEN_EXCEPTION(HttpStatus.UNAVAILABLE_FOR_LEGAL_REASONS, "구글 엑세스 토큰을 가져오는데 실패했습니다.");
 
     private final HttpStatus httpStatus;
     private final String message;
